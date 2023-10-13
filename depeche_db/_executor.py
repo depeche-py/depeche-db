fa633f2c471389753aff1da79b15b247734f03ee
Martin Vielsmaier <martin@vielsmaier.net>
2023-10-13 13:55:36 +0200

    Refactor executor

--- depeche_db/_executor.py
+++ depeche_db/_executor.py
@@ -2,71 +2,94 @@ import collections as _collections
 import signal as _signal
 import threading as _threading
 import time as _time
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional
 
 from ._interfaces import RunOnNotification
 from .tools import PgNotificationListener
 
 
 class Executor:
+    listener: Optional[PgNotificationListener] = None
+
     def __init__(self, db_dsn: str):
         self._db_dsn = db_dsn
         self.channel_register: Dict[
             str, List[Callable[[], None]]
         ] = _collections.defaultdict(list)
         self.stimulation_interval = 0.5
+        self.keep_running = True
+        self.handler_queue = HandlerQueue()
+        self.stimulator_thread = _threading.Thread(target=self.stimulate, daemon=True)
+        self.handler_thread = _threading.Thread(target=self.run_handlers, daemon=True)
+        self.listener = None
+        _signal.signal(_signal.SIGINT, lambda *_: self.stop())
+        _signal.signal(_signal.SIGTERM, lambda *_: self.stop())
 
     def register(self, handler: RunOnNotification):
         self.channel_register[handler.notification_channel].append(handler.run)
         return handler
 
+    def stop(self):
+        self.keep_running = False
+        if self.listener is not None:
+            self.listener.stop()
+
     def run(self):
-        listener = PgNotificationListener(
+        self.listener = PgNotificationListener(
             dsn=self._db_dsn,
             channels=list(self.channel_register),
             ignore_payload=True,
         )
-        keep_running = True
-        handler_queue: list[Callable[[], None]] = []
-        handler_queue_event = _threading.Event()
 
-        def stop():
-            print("Stopping...")
-            nonlocal keep_running
-            keep_running = False
-            listener.stop()
+        self.handler_thread.start()
+        self.stimulator_thread.start()
+        self.listener.start()
 
-        _signal.signal(_signal.SIGINT, lambda *_: stop())
-        _signal.signal(_signal.SIGTERM, lambda *_: stop())
-
-        def run_handlers():
-            while keep_running:
-                if handler_queue:
-                    handler = handler_queue.pop(0)
-                    handler()
-                else:
-                    handler_queue_event.wait()
-                    handler_queue_event.clear()
-
-        handler_thread = _threading.Thread(target=run_handlers, daemon=True)
-        handler_thread.start()
-
-        def stimulate():
-            while keep_running:
-                for handlers in self.channel_register.values():
-                    for handler in handlers:
-                        if handler not in handler_queue:
-                            handler_queue.append(handler)
-                            handler_queue_event.set()
-                _time.sleep(self.stimulation_interval)
-
-        stimulator = _threading.Thread(target=stimulate, daemon=True)
-        stimulator.start()
-
-        listener.start()
-        print("Started")
-        for notification in listener.messages():
+        for notification in self.listener.messages():
             for handler in self.channel_register[notification.channel]:
-                if handler not in handler_queue:
-                    handler_queue.append(handler)
-                    handler_queue_event.set()
+                self.handler_queue.put(handler)
+
+        self.handler_thread.join()
+        self.stimulator_thread.join()
+
+    def run_handlers(self):
+        while self.keep_running:
+            if self.handler_queue:
+                handler = self.handler_queue.get()
+                handler()
+            else:
+                self.handler_queue.wait()
+
+    def stimulate(self):
+        while self.keep_running:
+            for handlers in self.channel_register.values():
+                for handler in handlers:
+                    self.handler_queue.put(handler)
+            _time.sleep(self.stimulation_interval)
+
+
+class HandlerQueue:
+    def __init__(self):
+        self._queue: list[Callable[[], None]] = []
+        self._event = _threading.Event()
+
+    def put(self, handler: Callable[[], None]):
+        if handler not in self._queue:
+            self._event.set()
+            self._queue.append(handler)
+
+    @property
+    def should_wait(self):
+        return not self._event.is_set()
+
+    def wait(self):
+        self._event.wait()
+
+    def get(self) -> Callable[[], None]:
+        return self._queue.pop(0)
+
+    def __bool__(self):
+        return bool(self._queue)
+
+    def __len__(self):
+        return len(self._queue)
