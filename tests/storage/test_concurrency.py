6ae9560875e05c69559b9357045118914ea30f04
Martin Vielsmaier <martin@vielsmaier.net>
2023-10-19 20:41:29 +0200

    Test racing writes.

--- tests/storage/test_concurrency.py
+++ tests/storage/test_concurrency.py
@@ -1,8 +1,6 @@
 import threading as _threading
 import uuid as _uuid
 
-import pytest
-
 
 def test_concurrent_writes_to_separate_streams_work(db_engine, storage):
     with db_engine.connect() as conn1, db_engine.connect() as conn2:
@@ -15,16 +13,27 @@ def test_concurrent_writes_to_separate_streams_work(db_engine, storage):
         conn2.commit()
 
 
-@pytest.mark.skip("TODO how to test (does not terminate)")
 def test_concurrent_writes_to_same_stream_fail(db_engine, storage):
-    with db_engine.connect() as conn1, db_engine.connect() as conn2:
-        subject = storage
+    N = 5
+    success = []
 
-        subject.add(conn1, "stream1", 0, _uuid.uuid4(), {"foo": "bar"})
-        subject.add(conn2, "stream1", 0, _uuid.uuid4(), {"foo": "bar"})
+    def writer(n):
+        with db_engine.connect() as conn:
+            subject = storage
+            try:
+                subject.add(conn, "stream1", 0, _uuid.uuid4(), {"foo": "bar"})
+                conn.commit()
+                success.append(True)
+            except Exception:
+                pass
 
-        conn1.commit()
-        conn2.commit()
+    threads = [_threading.Thread(target=writer, args=(i,)) for i in range(N)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    assert len(success) == 1
 
 
 def test_multiple_writes_do_not_interfere(db_engine, storage):
