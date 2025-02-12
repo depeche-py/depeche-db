b37a71233e4442732c95bf48889a91850f350c26
Martin Vielsmaier <martin@vielsmaier.net>
2025-02-12 11:35:04 +0100

    Fix

--- tests/_tools.py
+++ tests/_tools.py
@@ -1,22 +1,24 @@
 import uuid as _uuid
 from urllib import parse as _urlparse
 
+import sqlalchemy as _sa
+
 
 def pg_check_if_db_exists(uri: str) -> bool:
-    conn = pg_get_connection(uri, use_postgres_db=True)
-    db_name = _get_db_name(uri)
-    count: int = conn.execute(
-        "select count(*) from pg_database where datname = %(db_name)s",
-        {"db_name": db_name},
-    ).fetchone()[0]
-    return count > 0
+    with pg_get_connection(uri, use_postgres_db=True) as conn:
+        db_name = _get_db_name(uri)
+        count: int = conn.execute(
+            _sa.text("select count(*) from pg_database where datname = :db_name"),
+            {"db_name": db_name},
+        ).scalar()
+        return count > 0
 
 
 def pg_create_db(uri: str):
     db_name = _get_db_name(uri)
     print(f"Creating database {db_name}")
     with pg_get_connection(uri, use_postgres_db=True) as conn:
-        conn.execute(f"CREATE DATABASE {db_name}")
+        conn.execute(_sa.text(f"CREATE DATABASE {db_name}"))
         print(f"Created database {db_name}")
 
 
@@ -24,22 +26,17 @@ def pg_drop_db(uri: str):
     db_name = _get_db_name(uri)
     print(f"Dropping database {db_name}")
     with pg_get_connection(uri, use_postgres_db=True) as conn:
-        conn.execute(f"DROP DATABASE {db_name}")
+        conn.execute(_sa.text(f"DROP DATABASE {db_name}"))
         print(f"Dropped database {db_name}")
 
 
 def pg_get_connection(uri: str, use_postgres_db: bool = False):
-    from depeche_db._compat import psycopg
-
     parts = list(_urlparse.urlparse(uri))
-    if parts[0] == "postgresql+psycopg" or parts[0] == "postgresql+psycopg2":
-        parts[0] = "postgresql"
     if use_postgres_db:
         parts[2] = "/postgres"
     uri = _urlparse.urlunparse(parts)
 
-    # autocommit=True is necessary for creating/dropping databases.
-    return psycopg.connect(uri, autocommit=True)
+    return _sa.create_engine(uri, isolation_level="AUTOCOMMIT").connect()
 
 
 def _get_db_name(uri: str) -> str:
