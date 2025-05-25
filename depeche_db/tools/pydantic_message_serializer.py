447000792d79c625f64dbfc194ef3a156b7cf7e7
Martin Vielsmaier <martin@vielsmaier.net>
2025-05-25 22:12:58 +0200

    Fix pydantic v1

--- depeche_db/tools/pydantic_message_serializer.py
+++ depeche_db/tools/pydantic_message_serializer.py
@@ -31,7 +31,7 @@ def _get_de_serializer(
         return type_.parse_obj(message)
 
     def serializer(message: T) -> dict:
-        return message.dict()
+        return make_jsonable(message.dict())
 
     return deserializer, serializer
 
