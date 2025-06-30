9bff84e4e3f99b9806f13b1f37d1cc52c60e7bbb
Martin Vielsmaier <martin@vielsmaier.net>
2025-06-30 20:32:15 +0200

    Fix teardown

--- tests/integration/test_full_chain.py
+++ tests/integration/test_full_chain.py
@@ -24,34 +24,42 @@ def test_it(pg_db, db_engine, stream_factory, store_factory, subscription_factor
         msg_sub_recv.append(time.time())
 
     subscription: Subscription = subscription_factory(stream, handlers=handlers)
+    executors = []
 
     def projector():
         executor = Executor(db_dsn=pg_db, disable_signals=True, stimulation_interval=0)
         executor.register(stream.projector)
+        executors.append(executor)
         executor.run()
 
     def sub():
         executor = Executor(db_dsn=pg_db, disable_signals=True, stimulation_interval=0)
         executor.register(subscription.runner)
+        executors.append(executor)
         executor.run()
 
-    threading.Thread(target=projector, daemon=True).start()
-    threading.Thread(target=sub, daemon=True).start()
-    time.sleep(0.5)
+    try:
+        threading.Thread(target=projector, daemon=True).start()
+        threading.Thread(target=sub, daemon=True).start()
+        time.sleep(0.5)
 
-    account_repo = AccountRepository(store)
-    account = Account.register(id=ACCOUNT1_ID, owner_id=uuid.uuid4(), number="123")
-    account.credit(100)
-    account_repo.save(account, expected_version=0)
-    first_save_done = time.time()
-    account.credit(100)
-    account_repo.save(account, expected_version=2)
-    second_save_done = time.time()
-    time.sleep(0.5)
+        account_repo = AccountRepository(store)
+        account = Account.register(id=ACCOUNT1_ID, owner_id=uuid.uuid4(), number="123")
+        account.credit(100)
+        account_repo.save(account, expected_version=0)
+        first_save_done = time.time()
+        account.credit(100)
+        account_repo.save(account, expected_version=2)
+        second_save_done = time.time()
+        time.sleep(0.5)
 
-    assert len(msg_sub_recv) == 3
-    # Timing
-    print(msg_sub_recv[0] - first_save_done)
-    print(msg_sub_recv[1] - second_save_done)
-    print(msg_sub_recv[2] - second_save_done)
-    # raise
+        assert len(msg_sub_recv) == 3
+        # Timing
+        print(msg_sub_recv[0] - first_save_done)
+        print(msg_sub_recv[1] - second_save_done)
+        print(msg_sub_recv[2] - second_save_done)
+        # raise
+
+    finally:
+        for executor in executors:
+            executor._stop()
